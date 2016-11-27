7b6d6a44c59006028cf8a98972cff9fc9b572798
Jay Lim <iejisung@gmail.com>
2016-11-27 20:47:17 +0900

    Commit from cli;

--- atoi.c
+++ atoi.c
@@ -3,17 +3,16 @@
 /* convert str to int */
 int atoi(char[]);
 
-int main()
-{
-  printf("2014 : %d\n", atoi("2014"));
+int main() {
+	printf("2014 : %d\n", atoi("2014"));
 }
 
-int atoi(char s[])
-{
-  int i;
-  int n = 0;
+int atoi(char s[]) {
+	int i;
+	int n = 0;
 
-  for (i = 0; s[i] >= '0' && s[i] <= '9'; ++i)
-    n = 10 * n + (s[i] - '0');
-  return n;
+	for (i = 0; s[i] >= '0' && s[i] <= '9'; ++i)
+		n = 10 * n + (s[i] - '0');
+
+	return n;
 }
